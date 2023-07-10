0923d500168de5fd634d741e71361fcd50bfa987
yjb23tech <yjb23tech@gmail.com>
2023-07-10 21:57:29 +0100

    Inspired by Oda

--- txt_game/sandbox_game.py
+++ txt_game/sandbox_game.py
@@ -1,1 +1,11 @@
-print("Over the course of the book, the mains of the book will invite me to add to the central game file; this will be done here")
+#print("Over the course of the book, the mains of the book will invite me to add to the central game file; this will be done here")
+
+print("\nEscape from Wano!")
+
+#Input func is used to record user input
+action_input = input("Samurai! Where to next? \n")
+
+#The user input provided is then printed back to the User
+print(f"\nYou're now travelling {action_input}! Be careful... there is danger ahead\n")
+
+
