cb28955d94be949f24672c773886b54592525bdf
yjb23tech <yjb23tech@gmail.com>
2023-07-10 21:15:19 +0100

    Back on track

---
+++ chp_2_wkbk/chp_2_hmwk.py
@@ -0,0 +1,18 @@
+#Homework: Requires you to set up a basic calc; all they want you to do is print text 
+#Will try and use my knowledge to see how far I can stretch it without needing help 
+
+print("This is a basic calculator; I will take user input and then return double the value provided")
+
+def calc_doubler(var_x):
+    doubled_val = var_x * 2 
+    return doubled_val 
+
+player_1_input = input("Please provide a number to be doubled: \n")
+player_1_input_int = int(player_1_input)
+response = calc_doubler(player_1_input_int)
+
+print(f"The value you provided was: {player_1_input_int}")
+print("\n")
+
+print(f"Your doubled value is: {response}")
+
