26e0ffa7b85fc20450c475e5805570dbf826ba8d
yjb23tech <yjb23tech@gmail.com>
2023-07-17 20:02:00 +0100

    Still Working

--- chp_4_wkbk_real/ages.py
+++ chp_4_wkbk_real/ages.py
@@ -3,4 +3,17 @@ print("\nHeil Ultron\n")
 ui_string_name = input("\nWho're you, Warrior?\n")
 print(f"\nYour name is {ui_string_name}? Son of Odin? I knew your father: he was a good man\n")
 
+arr_war_chest = ['Sword', 'Gauntlets', 'Hammer', 'Mjolnir'] 
+
+print("Use the number pad to choose your weapon\n")
+
+int_war_chest_counter = 1 
+
+for weapon in arr_war_chest: 
+    print(f"To wield the {weapon}, choose {int_war_chest_counter}")
+    int_war_chest_counter += 1 
+
+ui_int_war_chest_selection = int(input("\nChoose.\n"))
+
+print(f"I can see you have chosen {arr_war_chest[(ui_int_war_chest_selection) - 1]}; it is time to go to war {ui_string_name}, son of Odin")
 
