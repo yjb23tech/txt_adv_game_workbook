d09af28c19ff068089ba23017f74f8a6e099827d
yjb23tech <yjb23tech@gmail.com>
2023-07-17 19:55:25 +0100

    MCU Inspired

--- chp_4_wkbk_real/ages.py
+++ chp_4_wkbk_real/ages.py
@@ -1,1 +1,6 @@
-print("Heil Ultron")
+print("\nHeil Ultron\n")
+
+ui_string_name = input("\nWho're you, Warrior?\n")
+print(f"\nYour name is {ui_string_name}? Son of Odin? I knew your father: he was a good man\n")
+
+
