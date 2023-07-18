a7edde0363590d99c91fa75fa77bd12ca69a3aa3
yjb23tech <yjb23tech@gmail.com>
2023-07-18 22:48:30 +0100

    The Warrior's Journey Begins

--- chp_5_wkbk/game_remade_chp_5.py
+++ chp_5_wkbk/game_remade_chp_5.py
@@ -1,2 +1,12 @@
 print("Forward")
 
+def get_player_command(var_player_name):
+    var_player_command = input(f"\nWhere are you headed {var_player_name}? Be quick about it!\n")
+    return var_player_command 
+
+ui_player_name = input("\nWhat is your name Warrior?\n")
+
+ui_player_command = get_player_command(ui_player_name)
+
+print(f"\n{ui_player_command} it is {ui_player_name}... let us move with haste!\n")
+
