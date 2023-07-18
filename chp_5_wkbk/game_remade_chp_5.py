2f279ba41c933a0abce2b2e0e6d5f10c4ce75ffc
yjb23tech <yjb23tech@gmail.com>
2023-07-18 23:04:14 +0100

    Win

--- chp_5_wkbk/game_remade_chp_5.py
+++ chp_5_wkbk/game_remade_chp_5.py
@@ -1,12 +1,31 @@
-print("Forward")
+print("\nForward\n")
 
 def get_player_command(var_player_name):
     var_player_command = input(f"\nWhere are you headed {var_player_name}? Be quick about it!\n")
     return var_player_command 
 
-ui_player_name = input("\nWhat is your name Warrior?\n")
+def play():
 
-ui_player_command = get_player_command(ui_player_name)
+    print("\nTo Asgard!\n")
+    ui_player_name = input("\nWhat is your name Warrior?\n")
 
-print(f"\n{ui_player_command} it is {ui_player_name}... let us move with haste!\n")
+    ui_bool_game_is_on = True 
+
+    while(ui_bool_game_is_on == True):
+    
+        ui_player_command = get_player_command(ui_player_name)
+
+        if (ui_player_command.lower() == 'n') or (ui_player_command.lower() == 'north'):
+            print(f"\n{ui_player_command} it is {ui_player_name}... let us move with haste!\n")
+        elif (ui_player_command.lower() == 'e') or (ui_player_command.lower() == 'east'):
+            print(f"\n{ui_player_command}? Are you sure? As you wish...\n")
+        elif (ui_player_command.lower() == 'w') or (ui_player_command.lower() == 'west'):
+            print(f"\nThe Wicked Witch of the {ui_player_command} lies in this direction - we must keep our guard up Asgardian!\n")
+        elif (ui_player_command.lower() == 's') or (ui_player_command.lower() == 'south'):
+            print(f"\n{ui_player_command} promises nothing but misery {ui_player_name}\n")
+        else:
+            print(f"\nRest {ui_player_name}. Your Odyssey has come to end.\n")
+            ui_bool_game_is_on = False
+
+play()
 
