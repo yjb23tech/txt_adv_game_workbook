5a92faabaab422758796b557b4bdd97916f581fa
yjb23tech <yjb23tech@gmail.com>
2023-07-18 14:08:02 +0100

    Cannot get lazy

--- chp_5_wkbk/chp_5_wk_mains.py
+++ chp_5_wkbk/chp_5_wk_mains.py
@@ -1,1 +1,22 @@
-print("Anytime, anyplace")
+print("\nAnytime, anyplace\n")
+
+def say_hello(var_name):
+    print("\nSAY HELLO TO MA LUH FWEND\n " + var_name)
+
+say_hello('GID')
+
+ui_user_name = input("\nWho are you?\n")
+
+ui_bool_game_on = True
+
+while (ui_bool_game_on == True):
+    ui_chosen_one = input(f"\n{ui_user_name}... it cannot be! Are you, the child of prophecy?\n")
+
+    if (ui_chosen_one.lower() == 'y') or (ui_chosen_one.lower() == 'yes'):
+        say_hello(ui_user_name)
+    else:
+        print("Live another day, and never return")
+        ui_bool_game_on = False  
+
+
+
