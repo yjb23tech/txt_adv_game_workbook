c49858d674a48c57a8cb1a77e866be6deaf04ff9
yjb23tech <yjb23tech@gmail.com>
2023-07-19 18:05:21 +0100

    Slowly

--- chp_5_wkbk/chp_5_wk_hwk_calc.py
+++ chp_5_wkbk/chp_5_wk_hwk_calc.py
@@ -1,1 +1,48 @@
 print("Take your craft serious")
+
+def double(var_x):
+    var_doubled = var_x * 2
+    return var_doubled
+
+def add(var_x, var_y):
+    var_result_add = var_x + var_y 
+    return var_result_add 
+
+def subtract(var_x, var_y):
+    var_result_subtract = var_x - var_y 
+    return var_result_subtract
+
+def multiply(var_x, var_y):
+    var_result_multiply = var_x * var_y 
+    return var_result_multiply 
+
+def divide(var_x, var_y):
+    var_result_divide = (var_x / var_y)
+    return var_result_divide 
+
+ui_bool_calc_is_on = True 
+
+while (ui_bool_calc_is_on == True):
+    ui_calc_operation = input("\nWhat would you like to do?\n")
+
+    if (ui_calc_operation.lower() == 'double'):
+        print("\nYou would like to double your input?\n")
+    elif (ui_calc_operation.lower() == 'add'):
+        print("\nYou have chosen to add together a pair of values")
+    elif (ui_calc_operation.lower() == 'subtract'):
+        print("\nYou have chosen to subtract a value from another value\n") 
+    elif (ui_calc_operation.lower() == 'multiply'): 
+        print("\nYou have chosen to multiply a pair of values\n")
+    elif (ui_calc_operation.lower() == 'divide'):
+        print("\nYou have chosen to divide a pair of values\n")
+    else:
+        ui_calc_exit = input("\nYou have chosen an option out of the set range; are you sure you'd like to quit? Please press 'q' to confirm\n")
+
+        if (ui_calc_exit.lower() == 'q'):
+            print("Thank you for confirming your decision to turn off your calculator")
+            ui_bool_calc_is_on = False 
+        else:
+            continue
+
+print("\nGoodbye User\n")
+ 
