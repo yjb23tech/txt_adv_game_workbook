c32472e036fa176fbae68194cb36ce49532034c3
yjb23tech <yjb23tech@gmail.com>
2023-07-19 19:06:57 +0100

    Robust

--- chp_5_wkbk/chp_5_wk_hwk_calc.py
+++ chp_5_wkbk/chp_5_wk_hwk_calc.py
@@ -20,13 +20,23 @@ def divide(var_x, var_y):
     var_result_divide = (var_x / var_y)
     return var_result_divide 
 
+arr_calc_options = ['Double', 'Add', 'Subtract', 'Multiply', 'Divide']
+
 ui_bool_calc_is_on = True 
 
 while (ui_bool_calc_is_on == True):
+
+    print("\nThe following options are available to you:\n")
+
+    for option in arr_calc_options:
+        print(f"You can {option} values")
+    print("You can also Quit and turn the calculator off")
+    print("If you make a submission not recognised by the calculator, you will be presented with the options again and asked to provide a valid option")
+
     ui_calc_operation = input("\nWhat would you like to do?\n")
 
     if (ui_calc_operation.lower() == 'double'):
-        ui_var_x = int(input("\nYou have chosen to double your input; what value would you like to double\n"))
+        ui_var_x = int(input("\nYou have chosen to double your input; what value would you like to double?\n"))
         ui_var_x_doubled = double(ui_var_x)
         print(f"You chose to double {ui_var_x} and the result of this calculation is {ui_var_x_doubled} - congratulations!\n")
  
@@ -61,14 +71,18 @@ while (ui_bool_calc_is_on == True):
 
         ui_var_x_y_divided = divide(ui_var_x, ui_var_y)
         print(f"\nYou submitted {ui_var_x} and {ui_var_y} for division; the result of this calculation is {ui_var_x_y_divided}\n")
-    else:
-        ui_calc_exit = input("\nYou have chosen an option out of the set range; are you sure you'd like to quit? Please press 'q' to confirm\n")
+    elif (ui_calc_operation.lower() == 'quit'):
+        ui_calc_exit = input("\nYou have chosen to quit and turn your calculator off; are you sure you'd like to quit? Please press 'q' to confirm\n")
 
         if (ui_calc_exit.lower() == 'q'):
             print("Thank you for confirming your decision to turn off your calculator")
             ui_bool_calc_is_on = False 
         else:
+            print("\nYou will be presented with the calculator options again :D\n")
             continue
+    else:
+        print("\nYou have chosen an operation that is not recognised by this calculator program; please choose from the calculator options available\n")
+        continue
 
 print("\nGoodbye User\n")
  
