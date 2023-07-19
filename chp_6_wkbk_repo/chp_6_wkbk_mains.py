b40b7d01b2d389cd076bb30631184378a27df984
yjb23tech <yjb23tech@gmail.com>
2023-07-19 22:10:45 +0100

    Major Progress

--- chp_6_wkbk_repo/chp_6_wkbk_mains.py
+++ chp_6_wkbk_repo/chp_6_wkbk_mains.py
@@ -15,9 +15,30 @@ print("\nThe lates 80s and 90s was an amazing time for NBA basketball\n")
 for superstar_90s in arr_nba_superstars_90s:
     print(f"{superstar_90s} was the greatest at the {(arr_nba_superstars_90s.index(superstar_90s)) + 1} we'd ever seen up until the 2000s")
 
-print("\nBut this is a young man's game and the future is now\n")
+print("\nBut this is a young man's game and the modern era is here\n")
 for superstar_post_90s in arr_nba_superstars_post_90s:
     print(f"{superstar_post_90s} is now the modern greatest at the {(arr_nba_superstars_post_90s.index(superstar_post_90s)) + 1} - oh how times have changed!")
+
+#Using the .append() method 
+print("\nAll is not lost however - even now, as we speak, the league is undergoing tectonic shifts and a changing of the guard is imminent\n")
+
+#I declared and initialized an empty list (array) and then passed it into the len() function; I was accurately told the length i.e. size is 0 - I like that!
+arr_nba_superstars_future = []
+arr_nba_superstars_future_len = len(arr_nba_superstars_future)
+print(f"\nThe current roster for the new superstars contains {arr_nba_superstars_future_len} players; are you ready to change this?\n")
+
+var_arr_nba_superstars_future_counter = 0 
+
+while ((len(arr_nba_superstars_future)) <= 4):
+
+    ui_nba_superstars_future_selection = input(f"\nWho would you like to see at the {(var_arr_nba_superstars_future_counter) + 1} position?\n") 
+    arr_nba_superstars_future.append(ui_nba_superstars_future_selection)
+    print(f"\nSuper! You now have {ui_nba_superstars_future_selection} at the {(arr_nba_superstars_future.index(ui_nba_superstars_future_selection)) + 1} position!\n")
+    var_arr_nba_superstars_future_counter += 1 
+
+arr_nba_superstars_future_len = len(arr_nba_superstars_future)
+print(f"\nYour roster for future superstars now contains {arr_nba_superstars_future_len} - here's your starting {arr_nba_superstars_future_len}:\n")
+
 
 print("\nEnd\n") 
 
