0d014bfb124d828d2804d7e6d3e233a36b029b43
yjb23tech <yjb23tech@gmail.com>
2023-07-23 16:44:27 +0100

    This was a big success

--- chp_6_wkbk_repo/chp_6_hwk_middle_value.py
+++ chp_6_wkbk_repo/chp_6_hwk_middle_value.py
@@ -12,6 +12,15 @@ def middle_value(arr_odd_len):
 
     print(f"\nThis array has {len(arr_odd_len)} items\n")
 
+    if ((len(arr_odd_len) % 2) == 0):
+        print("\nThis array has an even number of items; we cannot at present accurately provide the middle value :S\n")
+    else:
+        print("\nThis array has an even number of items: we can provide the middle value :D\n")
+
+        middle_value_loc_lit = int((len(arr_odd_len) + 1) / 2)
+        middle_value_loc_index = int(middle_value_loc_lit - 1)
+
+        print(f"The middle item in this array is {arr_odd_len[middle_value_loc_index]}") 
 
 arr_test = ['Magic', 'Bird', 'Olajuwon', 'Jordan', 'Ewing']
 
