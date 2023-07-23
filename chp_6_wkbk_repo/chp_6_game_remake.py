6ea3e6e216bad7418e86c5cc110b40fe26b1711d
yjb23tech <yjb23tech@gmail.com>
2023-07-23 15:31:20 +0100

    Doing Well

--- chp_6_wkbk_repo/chp_6_game_remake.py
+++ chp_6_wkbk_repo/chp_6_game_remake.py
@@ -1,1 +1,28 @@
 print("Rebuilding the game once more, using details from Chp 6")
+
+def play():
+    inventory = ['Dagger', 'Gold(5)', 'Crusty Bread']
+    ui_bool_game_is_on = True
+
+    while (ui_bool_game_is_on == True):
+
+        ui_player_action = input("\nWhere to next Sailor?\n")
+
+        if ui_player_action in ['n', 'N', '^']:
+            print("You're now travelling North")
+        elif ui_player_action in ['e', 'E', '>']:
+            print("You're now travelling East")
+        elif ui_player_action in ['s', 'S']:
+            print("You're now travelling South")
+        elif ui_player_action in ['w', 'W', '<']:
+            print("You're now travelling West")
+        elif ui_player_action in ['q', 'Q']:
+            print("Journey well, Sailor")
+            ui_bool_game_is_on = False 
+        else:
+            print("Try again Sailor")
+
+
+play()
+print("Rest well, Sailor")
+    
