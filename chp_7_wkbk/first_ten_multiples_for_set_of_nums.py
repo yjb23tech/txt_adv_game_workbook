01eeaa77cf20ae96303a7a2f8355bed3598cf895
yjb23tech <yjb23tech@gmail.com>
2023-07-26 23:31:59 +0100

    Yep; definitely need better naming strat lol

--- chp_7_wkbk/first_ten_multiples_for_set_of_nums.py
+++ chp_7_wkbk/first_ten_multiples_for_set_of_nums.py
@@ -1,2 +1,11 @@
 print("Print the first 10 multiples for the first 5 non zero integers of the natural number line")
 
+print(" ") 
+
+for a in range(1,6):
+    for b in range(1,11):
+        print(f"{(a*b)}")
+
+    print(" ") 
+
+
