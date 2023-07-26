32193454c36a5b2e9bcf6a5d1fee89f3ff81068f
yjb23tech <yjb23tech@gmail.com>
2023-07-26 20:45:44 +0100

    This is a really good chapter to repeat multiple times; think of variations and try expand - really really good

--- chp_7_wkbk/favorites.py
+++ chp_7_wkbk/favorites.py
@@ -7,10 +7,21 @@ more_items = True
 def print_starred_list(arr_items_to_list):
 
     #[1] on Loop Counters: using an incrementable var outside of the for loop as a loop counter; once loop is run, var is incremented at very end and new value in i to be used during next loop iteration
-    i = 1   
-    for item in arr_items_to_list: 
-        print(f"*{i} " + str(item))
-        i += 1 
+    # i = 1   
+    #for item in arr_items_to_list: 
+        #print(f"*{i} " + str(item))
+        #i += 1 
+
+    #[2] on Loop Counters: uses the range() which takes an int [given by using len(arr) - which tells you the number of items in an arr aka the size] and then returns a zero indexed arr against the int
+    #This approach is very similar to my arr_example.index(item) + 1 approach; I actually prefer mine better; in this [2] instance the loop is run on the array returned by range(), not the primary arr
+    #You then use the indexes returned as the indices to feed into your get call on your primary arr 
+    #range() returns a zero indexed based array of individual items which spans the width of the initial int input; so if the input into range() is the int '5' you will get an arr: [0, 1, 2, 3, 4] 
+    #This returned range() object can acc be stored in a var and the var used as an arr within a standard for loop as normal
+    #Trick is to use the items stored in the arr as indices which map to corresponding items in the primary array fed into len() ==> which was then fed into ==> range() 
+    for i in range(len(arr_items_to_list)):
+        print(f"{i+1}. {arr_items_to_list[i]}")
+     
+
 
 while more_items:
         
@@ -38,5 +49,14 @@ else:
     print("\nYou have more than 5 items Sir! You'll have to join the back of the standard main cue I'm afraid!\n")
 
 print_starred_list(arr_favorites)
+
+#What can a range object actually do? Can it be assigned and stored in a variable which will act as a pointer to this range object i.e. modded array (list)? 
+
+arr_range_objects = range(len(arr_favorites))
+print(" ")
+for object in arr_range_objects:
+    print(f"And here we have a: {object}")
+
+#Who woulda thunk? Look UP ^^
 
 print("\nThank you for shopping with us today - come back soon!\n")
