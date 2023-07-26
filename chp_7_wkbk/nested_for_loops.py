30ab028b3a92e48b5cbd4013c999222b19e4df41
yjb23tech <yjb23tech@gmail.com>
2023-07-26 23:14:24 +0100

    Sandbox Settings

---
+++ chp_7_wkbk/nested_for_loops.py
@@ -0,0 +1,14 @@
+#print("Still growing; still learning")
+
+print(" ")
+
+for a in range(1,4):
+    for b in range(1,4):
+        print(f"{a} - here we go: {b}")
+    print(" ")
+
+
+#On the first iteration of loop (a), loop (b) runs 3 times 
+#On the second iteration of loop (a), loop (b) runs 3 times 
+#On the third iteration of loop (b), loop (b) runs 3 times
+ 
