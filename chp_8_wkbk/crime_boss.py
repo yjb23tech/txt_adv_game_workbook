58e66089026dac91a7d0066f03d8c13aa0abfa4f
yjb23tech <yjb23tech@gmail.com>
2023-07-29 16:57:47 +0100

    My idea worked

--- chp_8_wkbk/crime_boss.py
+++ chp_8_wkbk/crime_boss.py
@@ -1,4 +1,22 @@
 #Class Crime Boss will be created; using the __init__() method properties will be declared and initialized immediately so that upon Object creation these properties can be set immediately 
-print("Young Tony Stark")
+#print("Young Tony Stark")
+
+class Mob_Boss:
+    def __init__(self, str_name, int_age, arr_fav_foods):
+        self.str_name = str_name
+        self.int_age = int_age
+        self.arr_fav_foods = arr_fav_foods 
+
+def mb_first_name():
+    return input("\nWhat is your name?\n")
+
+def mb_age():
+    return int(input("\nHow old are you?\n"))
+
+mb_carmine_lupertazzi = Mob_Boss("Carmine Lupertazzi", 33, ['Seafood Risotto', 'Lasagna'])
+print(f"\nCiao Bella! My name is {mb_carmine_lupertazzi.str_name} and I am {mb_carmine_lupertazzi.int_age} years old\n")
+
+mb_player = Mob_Boss(mb_first_name(), mb_age(), ['King Prawn Linguine', 'Mozarella Balls'])
+print(f"\nMazel Tov! My name is {mb_player.str_name} and I am {mb_player.int_age} years old\n")
 
 
