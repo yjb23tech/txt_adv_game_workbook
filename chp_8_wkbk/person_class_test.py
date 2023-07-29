20eb0bea7bb4f059381403aa10ac44bb665c374c
yjb23tech <yjb23tech@gmail.com>
2023-07-29 13:57:23 +0100

    Silly error because of poor syntax

--- chp_8_wkbk/person_class_test.py
+++ chp_8_wkbk/person_class_test.py
@@ -4,8 +4,11 @@ class Person:
     age = 15 
     name = "Dmitri"
 
+    def birth_year(self):
+        return (2015 - self.age)
+
 russian_person = Person()
 
-print(f"Hello my name is {russian_person.name}")
-
+print(f"Hello my name is {russian_person.name}\n")
+print(f"I was born in the year {russian_person.birth_year()}")
 
