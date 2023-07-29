f7eb04853fda8d193644ce3614f4058a0429ebc3
yjb23tech <yjb23tech@gmail.com>
2023-07-29 16:48:48 +0100

    M A F I A

---
+++ chp_8_wkbk/crime_boss.py
@@ -0,0 +1,4 @@
+#Class Crime Boss will be created; using the __init__() method properties will be declared and initialized immediately so that upon Object creation these properties can be set immediately 
+print("Young Tony Stark")
+
+
