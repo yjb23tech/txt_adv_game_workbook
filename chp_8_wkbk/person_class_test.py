526d8d402a77fb06ef8ab5d27223b59aca0e7c52
yjb23tech <yjb23tech@gmail.com>
2023-07-29 14:01:04 +0100

    Learning OOP

--- chp_8_wkbk/person_class_test.py
+++ chp_8_wkbk/person_class_test.py
@@ -4,6 +4,9 @@ class Person:
     age = 15 
     name = "Dmitri"
 
+    #To use a Class based property within a Class based method, you need to pass the keyword 'self' into the method
+    #I imagine what this does is that it grants the Class based method access to all of the properties and methods within the class; it is effectively explicit self-referencing 
+    #The Class based method is explicitly instructed to access the described Class based property referenced i.e. use the 'age' property associated with the self-referencing object 
     def birth_year(self):
         return (2015 - self.age)
 
