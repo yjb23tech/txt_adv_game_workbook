5695d5449e0edc4001860504c6efc6632b4a979a
yjb23tech <yjb23tech@gmail.com>
2023-07-29 13:51:53 +0100

    From Moscow with Love

---
+++ chp_8_wkbk/person_class_test.py
@@ -0,0 +1,11 @@
+print("I will create a Class to model a Person")
+
+class Person:
+    age = 15 
+    name = "Dmitri"
+
+russian_person = Person()
+
+print(f"Hello my name is {russian_person.name}")
+
+
