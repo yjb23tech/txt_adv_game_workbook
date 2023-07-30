5d7ac49dbd4ce8e668ef62129090d193661b74a0
yjb23tech <yjb23tech@gmail.com>
2023-07-30 12:01:33 +0100

    Working through my progressions

--- warmups/xxx_vii/xxx_vii_sb.py
+++ warmups/xxx_vii/xxx_vii_sb.py
@@ -8,4 +8,20 @@ for x in range(1,8):
         print(f"{x*y}")
     print(" ") 
 
+#Print all of the factors for each number between 1 to 10 
+
+for x in range(1, 11):
+    arr_factors = [] 
+    print(f"Here are all of the factors of {x}")
+    for y in range(1, x+1): 
+        if ((x % y) == 0):
+            print(f"The number {y} is a factor of the number {x}")
+            arr_factors.append(y)
+        else:
+            print(f"The number {y} is not a factor of the number {x}")
+    print(f"\nHere are all of the factors of {x}: {arr_factors}\n")
+
+
+
+            
 
