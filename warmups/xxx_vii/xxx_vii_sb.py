44a3c47e9df360c6cb6d4cb2ce1423d151b14bed
yjb23tech <yjb23tech@gmail.com>
2023-07-30 11:50:32 +0100

    First 10 multiples of numbers 1 to 7

--- warmups/xxx_vii/xxx_vii_sb.py
+++ warmups/xxx_vii/xxx_vii_sb.py
@@ -1,1 +1,11 @@
 print("Sandbox actvities")
+
+#First 10 multiples of numbers 1 to 7 
+
+for x in range(1,8):
+    print(f"Here are the first 10 multiples of {x}:\n")
+    for y in range(1, 11):
+        print(f"{x*y}")
+    print(" ") 
+
+
