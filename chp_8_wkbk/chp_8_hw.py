2fb20716c188a9e9d5656d6b1da4dc960cd0d282
yjb23tech <yjb23tech@gmail.com>
2023-08-10 21:02:50 +0100

    Plato's Cave

--- chp_8_wkbk/chp_8_hw.py
+++ chp_8_wkbk/chp_8_hw.py
@@ -10,6 +10,11 @@ class Food:
     def __str__(self):
         return (f"This piece of {self.name} has {self.carbs} grams of carbs, {self.protein} grams of protein and {self.fat} grams of fat")
 
+    def total_calorie_count(self):
+        total_calories = (self.carbs * 4) + (self.protein * 4) + (self.fat * 9)
+        return str(f"If you were to consume this piece of {self.name} you would be eating {total_calories} worth of calories")
+
 test_chicken_thigh = Food("Chicken Thigh", 100, 200, 300)
 
 print(test_chicken_thigh)
+print(test_chicken_thigh.total_calorie_count())
