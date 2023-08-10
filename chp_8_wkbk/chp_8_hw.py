e62aa0cead1072829709e35f1f6e6f44cb5e5531
yjb23tech <yjb23tech@gmail.com>
2023-08-10 20:24:22 +0100

    Could turn into my own version of myfitnessapp lol

--- chp_8_wkbk/chp_8_hw.py
+++ chp_8_wkbk/chp_8_hw.py
@@ -1,1 +1,15 @@
-print("Still completing homework at age 29")
+#print("Still completing homework at age 29")
+
+class Food:
+    def __init__(self, name, carbs, protein, fat):
+        self.name = name
+        self.carbs = carbs
+        self.protein = protein 
+        self.fat = fat
+
+    def __str__(self):
+        return (f"This piece of {self.name} has {self.carbs} grams of carbs, {self.protein} grams of protein and {self.fat} grams of fat")
+
+test_chicken_thigh = Food("Chicken Thigh", 100, 200, 300)
+
+print(test_chicken_thigh)
